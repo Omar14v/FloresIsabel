dbd0cdeb6586c48c055a487a819d6ea6e9c3dcd3
Omar <gary.veloz@ister.edu.ec>
2020-06-20 18:16:15 -0500

    Verificación Login
    1) envio de email para combrobar
    2) despliegue de información si ha iniciado correctamente
    3) cerrar sesión

---
+++ login/app.js
@@ -0,0 +1,111 @@
+function ingresar(){
+
+    var correo = document.getElementById('idcorreo').value;
+    var contraseña = document.getElementById('idcontraseña').value;
+
+    firebase.auth().signInWithEmailAndPassword(correo, contraseña)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+
+function registrar(){
+    var correo2 = document.getElementById('idcorreo2').value;
+    var contraseña2 = document.getElementById('idcontraseña2').value;
+
+    firebase.auth().createUserWithEmailAndPassword(correo2, contraseña2)
+    .then(function(){
+        verificar() 
+        alert("Verifique su cuenta y recargue esta página")
+    })
+
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+}
+
+function observar(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('Existe usuario activo')
+            alert("Ha iniciado sesión")
+            aparece(user);
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+
+          console.log('*****************************');
+          console.log(user.emailVerified)
+          console.log('*****************************');
+
+          console.log(email);
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          console.log('No existe usuario activo')
+          // ...
+        }
+      });
+}
+observar();
+
+//Contenido
+function aparece(user){
+    var user = user;
+    var contenido = document.getElementById('contenido');
+    if(user.emailVerified){
+
+        contenido.innerHTML = `
+    
+        <p>Bienvenido</p>
+        <button onclick="cerrar()">Cerrar Sesión</button>
+
+    `;
+
+    }
+    
+}
+
+function cerrar(){
+    firebase.auth().signOut()
+    .then(function(){
+        console.log('Saliendo...')
+        alert("Ha cerrado sesión")
+
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+}
+
+//Verificar
+function verificar(){
+    var user = firebase.auth().currentUser;
+
+    user.sendEmailVerification().then(function() {
+    // Email sent.
+        console.log('Enviando correo...');
+    }).catch(function(error) {
+    // An error happened.
+        console.log(error);
+        
+    });
+}
